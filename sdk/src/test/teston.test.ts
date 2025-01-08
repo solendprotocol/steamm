1c64eab9b77737bc7b61c6974aea9215bf33e4fe
0xxgen1 <0xxgen@solend.fi>
2025-01-08 14:48:10 +0000

    localnet testing pipeline

--- sdk/src/test/teston.test.ts
+++ sdk/src/test/teston.test.ts
@@ -1,16 +1,10 @@
 import { assert, expect } from "chai";
 import { SUILEND_TESTNET_PKG, TESTNET_REGISTRY } from "../consts";
 import { Transaction } from "@mysten/sui/transactions";
-// import { JsonRpcProvider, Ed25519Keypair, RawSigner } from "@mysten/sui.js";
 import { Ed25519Keypair } from "@mysten/sui/keypairs/ed25519";
 import { getFullnodeUrl, SuiClient } from "@mysten/sui/dist/cjs/client";
 import { Bank } from "../bank/bank";
-// import { getFullnodeUrl, SuiClient } from "@mysten/sui/client";
-// import { getFullnodeUrl, SuiClient } from "@mysten/sui/dapp_kit";
-// import {
-//   signAndExecuteTransaction,
-//   signTransaction,
-// } from "@mysten/wallet-standard";
+import { describe, it, beforeAll, beforeEach, afterAll } from "@jest/globals";
 
 export function test() {
   describe("describe", () => {
@@ -20,22 +14,23 @@ export function test() {
 
     afterAll(async () => {});
 
-    it("it", async () => {
+    it("should create a keypair", async () => {
       const keypair = new Ed25519Keypair();
+      expect(keypair).to.be.an("object");
       // const signer = new RawSigner(keypair, provider);
 
-      const tx = new Transaction();
+      // const tx = new Transaction();
 
-      Bank.createBank(
-        `${SUILEND_TESTNET_PKG}::lending_market::LENDING_MARKET`,
-        `0x0::sui::SUI`,
-        TESTNET_REGISTRY,
-        tx
-      );
+      // Bank.createBank(
+      //   `${SUILEND_TESTNET_PKG}::lending_market::LENDING_MARKET`,
+      //   `0x0::sui::SUI`,
+      //   TESTNET_REGISTRY,
+      //   tx
+      // );
 
-      const client = new SuiClient({ url: getFullnodeUrl("testnet") });
-      const bytes = await tx.build({ client });
-      const { signature } = await keypair.signTransaction(bytes);
+      // const client = new SuiClient({ url: getFullnodeUrl("localnet") });
+      // const bytes = await tx.build({ client });
+      // const { signature } = await keypair.signTransaction(bytes);
 
       //   const { digest, effects } = await client.signAndExecuteTransaction(
       //     {
